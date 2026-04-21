045a6d91e779aa3c4d46d6f8ba0ba867f7438f0f
saromoAndreiLuis <165786140+saromoAndreiLuis@users.noreply.github.com>
2026-04-21 20:15:47 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 // Configuration for the Community Outreach Registration System
 export const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyvw6kIB0e_YFUGuVeSf9G67GRGUOHzRvVFSjF16SV4d2nxueVwJjMz8P5PfWiPKAJTUw/exec'; //0.0.9 script
-export const APP_VERSION = 'v0.0.8';
+export const APP_VERSION = 'v0.0.9';
